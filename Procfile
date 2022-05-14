9dbc6ee3f72586484263e5c43bf712fda720ff88
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2022-05-14 14:10:41 +0300

    Refactoring server for heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/spring-boot-security-postgresql-0.0.1-SNAPSHOT.jar
--- src/main/java/com/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
+++ src/main/java/com/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
@@ -1,13 +1,36 @@
 package com.spring.security.postgresql;
 
+import com.spring.security.postgresql.models.ERole;
+import com.spring.security.postgresql.models.Role;
+import com.spring.security.postgresql.repository.RoleRepository;
+import com.spring.security.postgresql.repository.UserRepository;
+
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class SpringBootSecurityPostgresqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(RoleRepository roleRepository, UserRepository userRepository) {
+		if (roleRepository.findAll().size() == 0) {
+			roleRepository.saveAll(Arrays.asList(
+					new Role(ERole.ROLE_ADMIN),
+					new Role(ERole.ROLE_USER)
+			));
+		}
+		return args -> {
+		};
 	}
 
 }
